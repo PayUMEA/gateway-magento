6aeb75bd32a7b97468b0502a5cc6529b6011aaaf
Kenneth Madu Onah <kenneth@nanotech-solutions.co.za>
2023-04-06 16:03:18 +0200

    Add transfer object factory. Transfer object is composed of request builder data.

---
+++ Gateway/Http/TransferFactory.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright © 2022 PayU Financial Services. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace PayU\Gateway\Gateway\Http;
+
+use Magento\Payment\Gateway\Http\TransferBuilder;
+use Magento\Payment\Gateway\Http\TransferFactoryInterface;
+use Magento\Payment\Gateway\Http\TransferInterface;
+
+/**
+ * class TransferFactory
+ * @package PayU\Gateway\Gateway\Http
+ */
+class TransferFactory implements TransferFactoryInterface
+{
+    /**
+     * @var TransferBuilder
+     */
+    private TransferBuilder $transferBuilder;
+
+    /**
+     * @param TransferBuilder $transferBuilder
+     */
+    public function __construct(
+        TransferBuilder $transferBuilder
+    ) {
+        $this->transferBuilder = $transferBuilder;
+    }
+
+    /**
+     * Builds gateway transfer object
+     *
+     * @param array $request
+     * @return TransferInterface
+     */
+    public function create(array $request): TransferInterface
+    {
+        return $this->transferBuilder
+            ->setBody($request)
+            ->build();
+    }
+}
