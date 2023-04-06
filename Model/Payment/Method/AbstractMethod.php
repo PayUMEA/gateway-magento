1683df097f6bc085509889b7054d280d3ae79324
Kenneth Madu Onah <kenneth@nanotech-solutions.co.za>
2023-04-06 06:27:05 +0200

    Declare payment method codes

---
+++ Model/Payment/Method/AbstractMethod.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © 2022 PayU Financial Services. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace PayU\Gateway\Model\Payment\Method;
+
+/**
+ * class AbstractMethod
+ * @package PayU\Gateway\Model\Payment\Method
+ */
+class AbstractMethod
+{
+    public const CODE = 'no_code';
+}
---
+++ Model/Payment/Method/Creditcard.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © 2022 PayU Financial Services. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace PayU\Gateway\Model\Payment\Method;
+
+/**
+ * Copyright © 2022 PayU Financial Services. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+class Creditcard extends AbstractMethod
+{
+    public const CODE = 'payu_gateway_creditcard';
+}
