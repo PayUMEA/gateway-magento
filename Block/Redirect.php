847c39ebdb79b5795e114c38cc8897aed337aa55
Kenneth Madu Onah <kenneth@nanotech-solutions.co.za>
2023-04-06 16:13:35 +0200

    Add view model for redirect to PayU gateway in storefront

---
+++ Block/Redirect.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright © 2022 PayU Financial Services. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace PayU\Gateway\Block;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Payment\Block\Info;
+use PayU\EasyPlus\Helper\Backend\Data;
+use PayU\EasyPlus\Helper\DataFactory;
+
+/**
+ * class Redirect
+ * @package PayU\Gateway\Block
+ */
+class Redirect extends Info
+{
+    /**
+     * @var DataFactory
+     */
+    protected DataFactory $dataFactory;
+
+    /**
+     * @var ManagerInterface
+     */
+    private ManagerInterface $messageManager;
+
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param DataFactory $dataFactory
+     * @param ManagerInterface $messageManager
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        DataFactory $dataFactory,
+        ManagerInterface $messageManager,
+        array $data = []
+    ) {
+        $this->dataFactory = $dataFactory;
+        $this->messageManager = $messageManager;
+
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Get helper data
+     *
+     * @param string $area
+     * @return Data|\PayU\EasyPlus\Helper\Data
+     *@throws LocalizedException
+     */
+    public function getHelper(string $area): Data|\PayU\EasyPlus\Helper\Data
+    {
+        return $this->dataFactory->create($area);
+    }
+
+    /**
+     * {inheritdoc}
+     */
+    protected function _beforeToHtml(): Redirect
+    {
+        $this->addSuccessMessage();
+
+        return parent::_beforeToHtml();
+    }
+
+    /**
+     * Add success message
+     *
+     * @return void
+     */
+    private function addSuccessMessage(): void
+    {
+        $params = $this->getParams();
+
+        if (isset($params['PayUReference'])) {
+            $this->messageManager->addSuccessMessage(__('Redirecting to payment gateway...Please wait'));
+        }
+    }
+}
