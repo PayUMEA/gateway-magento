53e64b8a6f624efb5b02136c2bea756b24773431
Kenneth Madu Onah <kenneth@nanotech-solutions.co.za>
2023-07-15 17:53:38 +0200

    Add 'New' state

--- Model/Constants/TransactionState.php
+++ Model/Constants/TransactionState.php
@@ -14,6 +14,7 @@ namespace PayU\Gateway\Model\Constants;
  */
 enum TransactionState: string
 {
+    case NEW = 'NEW';
     case PROCESSING = 'PROCESSING';
     case SUCCESSFUL = 'SUCCESSFUL';
     case FAILED = 'FAILED';
