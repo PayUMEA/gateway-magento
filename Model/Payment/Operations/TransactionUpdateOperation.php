950caf42586ac648e65d03cedd61376bcb7796a1
Kenneth Madu Onah <kenneth@nanotech-solutions.co.za>
2023-07-15 17:53:12 +0200

    Refactor the process of updating payment transaction table. Merge existing data with new data and add reference.

--- Model/Payment/Operations/TransactionUpdateOperation.php
+++ Model/Payment/Operations/TransactionUpdateOperation.php
@@ -9,12 +9,15 @@ declare(strict_types=1);
 namespace PayU\Gateway\Model\Payment\Operations;
 
 use Magento\Framework\DataObject;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Payment\Model\InfoInterface;
+use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\Data\TransactionInterface;
 use Magento\Sales\Api\OrderPaymentRepositoryInterface;
 use Magento\Sales\Api\TransactionRepositoryInterface;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\Payment\Transaction\BuilderInterface;
+use PayU\Gateway\Gateway\Config\Config;
 
 /**
  * class TransactionUpdateOperation
@@ -23,11 +26,13 @@ use Magento\Sales\Model\Order\Payment\Transaction\BuilderInterface;
 class TransactionUpdateOperation
 {
     /**
+     * @param Config $config
      * @param BuilderInterface $transactionBuilder
      * @param OrderPaymentRepositoryInterface $paymentRepository
      * @param TransactionRepositoryInterface $transactionRepository
      */
     public function __construct(
+        protected readonly Config $config,
         protected readonly BuilderInterface $transactionBuilder,
         protected OrderPaymentRepositoryInterface $paymentRepository,
         protected TransactionRepositoryInterface $transactionRepository
@@ -39,33 +44,54 @@ class TransactionUpdateOperation
      * @param InfoInterface $payment
      * @param DataObject $transactionInfo
      * @return void
+     * @throws LocalizedException
      */
     public function update(
-        Order $order,
+        OrderInterface $order,
         InfoInterface $payment,
         DataObject $transactionInfo
     ): void {
         $transactionBuilder = $this->transactionBuilder->setPayment($payment);
         $transactionBuilder->setOrder($order);
         $transactionBuilder->setFailSafe(true);
-        $transactionBuilder->setTransactionId($payment->getTransactionId());
-        $transactionBuilder->setAdditionalInformation(
-            [
-                Order\Payment\Transaction::RAW_DETAILS => $transactionInfo->getPaymentData()
-            ]
-        );
+        $transactionBuilder->setTransactionId($transactionInfo->getPayUReference());
 
         $formattedPrice = $order->getBaseCurrency()->formatTxt(
             $order->getGrandTotal()
         );
         $message = __('The transaction amount is %1.', $formattedPrice);
 
-        $transaction = $transactionBuilder->build(TransactionInterface::TYPE_ORDER);
+        $payment->setIsTransactionClosed(true);
+        $payment->setShouldCloseParentTransaction(true);
+        $transactionBuilder->setMessage($message);
+        $transaction = $transactionBuilder->build($this->getPaymentAction((int)$order->getStoreId()));
+        $transaction->setAdditionalInformation(
+            Order\Payment\Transaction::RAW_DETAILS,
+            $transaction->getAdditionalInformation(
+                Order\Payment\Transaction::RAW_DETAILS
+            ) + $transactionInfo->getPaymentData()
+        );
         $payment->addTransactionCommentsToOrder(
             $transaction,
             $message
         );
+
         $this->paymentRepository->save($payment);
         $this->transactionRepository->save($transaction);
+    }
+
+    /**
+     * @param int $storeId
+     * @return string
+     */
+    private function getPaymentAction(int $storeId): string
+    {
+        $transactionType = $this->config->getTransactionType($storeId);
+
+        return match ($transactionType) {
+            'order',  => TransactionInterface::TYPE_ORDER,
+            'authorize' => TransactionInterface::TYPE_AUTH,
+            'authorize_capture' => TransactionInterface::TYPE_CAPTURE
+        };
     }
 }
